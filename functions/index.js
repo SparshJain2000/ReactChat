7afb2af1a5b6d4aef0b7635bb91777443490b8d5
SparshJain2000 <jainsparsh0801@gmail.com>
2020-10-24 02:14:28 +0530

    getAllUser function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const Filter = require("bad-words");
 const admin = require("firebase-admin");
+// const cors = require("cors")({ origin: true });
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -9,6 +10,37 @@ admin.initializeApp();
 const moreBadWords = ["chutia", "loda", "lodu", "gandu", "bc"];
 console.log("INSODE");
 const db = admin.firestore();
+exports.getAllUsers = functions.https.onRequest((req, res) => {
+    res.set("Access-Control-Allow-Origin", "*");
+
+    if (req.method === "OPTIONS") {
+        // Send response to OPTIONS requests
+        res.set("Access-Control-Allow-Methods", "GET");
+        res.set("Access-Control-Allow-Headers", "Content-Type");
+        res.set("Access-Control-Max-Age", "3600");
+    }
+    // res.set("Access-Control-Allow-Credentials", "true");
+    admin
+        .auth()
+        .listUsers()
+        .then(function (listUsersResult) {
+            console.log(listUsersResult);
+
+            return res.json(listUsersResult.users);
+
+            // listUsersResult.users.forEach(function (userRecord) {
+            //     res.json(userRecord.toJSON());
+            // });
+            // if (listUsersResult.pageToken) {
+            //     // List next batch of users.
+            //     listAllUsers(listUsersResult.pageToken);
+            // }
+        })
+        .catch(function (error) {
+            console.log("Error listing users:", error);
+        });
+});
+
 exports.detectEvilUsers = functions.firestore
     .document("messages/{msgId}")
     .onCreate(async (doc, ctx) => {
