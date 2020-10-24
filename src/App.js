1da4e11fbd68b2cda6de2da5ecc1c0d97748735c
SparshJain2000 <jainsparsh0801@gmail.com>
2020-10-24 13:56:51 +0530

    navbar component + UI changes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,7 @@ import {
     Button,
     TextField,
     Zoom,
-    AppBar,
-    Drawer,
-    IconButton,
-    Toolbar,
-    Typography,
+    Grow,
     Avatar,
     List,
     ListItem,
@@ -27,11 +23,11 @@ import {
     Grid,
     SvgIcon,
 } from "@material-ui/core";
-import { createMuiTheme, withStyles } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import { Telegram, ExitToApp, Facebook, Menu } from "@material-ui/icons";
-import logo from "./fire.svg";
+import { Telegram, Facebook } from "@material-ui/icons";
 import Axios from "axios";
+import Navbar from "./components/navbar.component";
 firebase.initializeApp({
     apiKey: "AIzaSyC5bjSXgNT4ME3G1CtMECI2Ee2d2fLIMJQ",
     authDomain: "reactchat-d97e5.firebaseapp.com",
@@ -51,7 +47,6 @@ const theme = createMuiTheme({
         secondary: { main: "#e16428" },
         error: { main: "#bb2205" },
         success: { main: "#132743" },
-        // background: { main: "#414141" },
     },
 });
 const auth = firebase.auth();
@@ -79,7 +74,7 @@ function App() {
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <div className='flex-column'>
-                <Navbar user={user} users={users} />
+                <Navbar user={user} users={users} auth={auth} />
                 <Grid container className='f-grow-1 h-90 mt-10'>
                     <Snackbar
                         style={{ width: "90vw" }}
@@ -98,7 +93,7 @@ function App() {
                         xs={12}
                         sm={10}
                         md={8}
-                        lg={6}
+                        lg={7}
                         className='mx-auto bg-darker'>
                         {user ? (
                             <ChatRoom />
@@ -114,78 +109,7 @@ function App() {
         </ThemeProvider>
     );
 }
-const UserList = ({ users }) => {
-    return (
-        <List className='p-1'>
-            <Typography variant='h6' className='align-center'>
-                Users
-            </Typography>
-            {users.map((user) => (
-                <ListItem
-                    className={`${"row"}`}
-                    alignItems='flex-start'
-                    key={user.uid}>
-                    <ListItemAvatar className='my-auto'>
-                        <Avatar size='small' src={user.photoURL} alt='' />
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary={user.displayName}
-                        className='w-max my-auto font-open'
-                    />
-                </ListItem>
-            ))}
-        </List>
-    );
-};
-const Navbar = ({ user, users }) => {
-    const [isOpen, setOpen] = useState(false);
-    const toggleDrawer = () => {
-        setOpen(!isOpen);
-    };
 
-    return (
-        <AppBar position='fixed' color='primary'>
-            <Toolbar>
-                {user && (
-                    <React.Fragment key={"left"}>
-                        <IconButton
-                            edge='start'
-                            variant='temporary'
-                            // className={classes.menuButton}
-                            onClick={toggleDrawer}
-                            color='inherit'
-                            aria-label='menu'>
-                            <Menu />
-                        </IconButton>
-                        <Drawer
-                            anchor={"left"}
-                            open={isOpen}
-                            onClose={toggleDrawer}>
-                            <UserList users={users} />
-                        </Drawer>
-                    </React.Fragment>
-                )}
-                <img src={logo} style={{ height: "1.5rem" }} />
-                <Typography
-                    variant='h6'
-                    className='f-grow-1 align-left font-raleway ml-2'>
-                    ReactChat
-                </Typography>
-                {user ? (
-                    <SignOut />
-                ) : (
-                    <Button
-                        color='inherit'
-                        size='small'
-                        href='http://sparshjain.me'
-                        target='_blank'>
-                        {"</> with 🧡 by Sparsh"}
-                    </Button>
-                )}
-            </Toolbar>
-        </AppBar>
-    );
-};
 const SignIn = ({ handleErr, handleOpen }) => {
     const signInFB = () => {
         const provider = new firebase.auth.FacebookAuthProvider();
@@ -197,16 +121,13 @@ const SignIn = ({ handleErr, handleOpen }) => {
                 // The signed-in user info.
                 var user = result.user;
                 // ...
-                console.log(user, token);
+                console.log(user);
             })
             .catch((error) => {
                 // Handle Errors here.
-                var errorCode = error.code;
                 var errorMessage = error.message;
                 // The email of the user's account used.
-                var email = error.email;
                 // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
                 handleErr(errorMessage);
                 handleOpen(true);
                 console.log(error);
@@ -218,7 +139,6 @@ const SignIn = ({ handleErr, handleOpen }) => {
         auth.signInWithPopup(provider)
             .then((result) => {
                 // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-                var token = result.credential.accessToken;
                 // The signed-in user info.
                 var user = result.user;
                 // ...
@@ -226,12 +146,9 @@ const SignIn = ({ handleErr, handleOpen }) => {
             })
             .catch((error) => {
                 // Handle Errors here.
-                var errorCode = error.code;
                 var errorMessage = error.message;
                 // The email of the user's account used.
-                var email = error.email;
                 // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
                 handleErr(errorMessage);
                 handleOpen(true);
                 console.log(error);
@@ -267,19 +184,7 @@ const SignIn = ({ handleErr, handleOpen }) => {
         </div>
     );
 };
-const SignOut = () => {
-    return (
-        auth.currentUser && (
-            <Button
-                color='secondary'
-                variant='contained'
-                onClick={() => auth.signOut()}
-                endIcon={<ExitToApp />}>
-                Sign Out
-            </Button>
-        )
-    );
-};
+
 const ChatRoom = () => {
     const xtra = useRef();
     const messRef = firestore.collection("messages");
@@ -340,24 +245,22 @@ const ChatRoom = () => {
 const ChatMess = ({ msg }) => {
     const { text, uid, photoURL } = msg;
     return (
-        <Zoom in={true} style={{ transitionDelay: "600ms" }}>
+        <Grow in={true} style={{ transitionDelay: "300ms" }}>
             {/* <div className='bg-secondary'> */}
             <ListItem
                 className={`${
                     uid !== auth.currentUser.uid ? "row" : "row-reverse"
                 }`}
                 alignItems='flex-start'>
-                <ListItemAvatar className='my-auto'>
-                    <Avatar src={photoURL} alt='' />
+                <ListItemAvatar className='my-auto '>
+                    <Avatar src={photoURL} alt='' className='mx-auto' />
                 </ListItemAvatar>
                 <ListItemText
                     primary={text}
                     className='w-max my-auto p-2 m-1 mx-1 wrap bg-secondary font-open'
                 />
-                {/* <p>{text}</p> */}
             </ListItem>
-            {/* </div> */}
-        </Zoom>
+        </Grow>
     );
 };
 const GoogleIcon = () => {
